30da1d1283f2b912deaa73e13f8208b8322993fa
Amar Trebinjac <amartrebinjac@gmail.com>
2018-05-01 20:41:21 +0200

    Kanskje lurt å legge til en eventlistener til begge knappene.

--- js/_site.js
+++ js/_site.js
@@ -1,6 +1,12 @@
 
+
+
+
+
 // Listens to click, replaces the choices with stats of what people chose
 document.getElementById('choiceOne').addEventListener('click', showStats)
+document.getElementById('choiceTwo').addEventListener('click', showStats)
+
 
 // the Stat swap function
 function showStats() {
