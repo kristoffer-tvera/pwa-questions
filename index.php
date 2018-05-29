b9219426d4f507f2c689bfd2a8dbf03a04c98c6b
Amar Trebinjac <amartrebinjac@gmail.com>
2018-05-29 12:24:27 +0200

    Oppdaterte CSS på knapper og endret fra <button> til <a>

--- index.php
+++ index.php
@@ -1,16 +1,50 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- <?php 
- include 'php/db_connect.php';
-//  include 'php/question.php';
-//  $question1 = new Question("Food", "Pepperoni", 0, "Salami", 5);
+<!doctype html>
+<html lang="en">
 
- echo json_encode(Get_question_random());
- //echo Add_question("Lifting", "Running", "Sports");
- ?>
- </body>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="css/site.min.css">
+
+    <title>This or That</title>
+</head>
+
+<body>
+    <div class="gameChoices__container">
+        <div class="gameChoices__background">
+            <button class="gameChoices__background--button" id="choiceOne">
+                <h1>Placeholder Text For Choice 1</h1>
+            </button>
+        </div>
+        <div class="gameChoices__background">
+            <button class="gameChoices__background--button" id="choiceTwo">
+                <h1>Placeholder Text For Choice 2</h1>
+            </button>
+        </div>
+
+        <div class="bottomBar">
+            <a href='questionCreator.html' target='_blank' class="bottomBar__menuBtn">
+                <h5>Make your own!</h5>
+            </a>
+            <a class="bottomBar__menuBtn">
+                <h5>Categories</h5>
+            </a>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+    <script src="js/_site.js"></script>
+</body>
+
 </html>
