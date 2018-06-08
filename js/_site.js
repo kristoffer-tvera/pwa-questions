4f35ff04a4b27e6de807e12072b92ac71eca408b
Kristoffer Tverå <Bluekristoffer@gmail.com>
2018-06-08 19:04:50 +0200

    post request for å vote

--- js/_site.js
+++ js/_site.js
@@ -28,6 +28,7 @@ xmlhttp.open(method, url, true);
 xmlhttp.onload = function questions() {
     // Laster inn API-calls
     var response = JSON.parse(this.responseText);
+    var id = response.id;
     var altOne = response.first_alternative;
     var altOneScore = response.first_alternative_score;
     var altTwo = response.second_alternative;
@@ -45,19 +46,36 @@ xmlhttp.onload = function questions() {
 
 
     // Hører etter clicks, for og så erstatte spørsmålet med statistikken
-    document.getElementById('choiceOne').addEventListener('click', showStats)
-    document.getElementById('choiceTwo').addEventListener('click', showStats)
+    document.getElementById('choiceOne').addEventListener('click', function (e) {
+        postAnswer(id, true, altOneScore, altTwoScore);
+    });
 
-    // the Stat swap function
-    function showStats() {
+    document.getElementById('choiceTwo').addEventListener('click', function (e) {
+        postAnswer(id, false, altOneScore, altTwoScore);
+    });
 
-        document.getElementById('choiceOne').innerHTML = altOneScore;
 
-        document.getElementById('choiceTwo').innerHTML = altTwoScore;
-    }
 
 }
 
 xmlhttp.send();
 
 
+//post the answer to a question
+function postAnswer(id, first, firstScore, secondScore) {
+    document.getElementById('choiceOne').innerHTML = firstScore;
+    document.getElementById('choiceTwo').innerHTML = secondScore;
+
+    var xhr = new XMLHttpRequest();
+    var url = "/api/questions/update.php";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            console.log(json.email + ", " + json.password);
+        }
+    };
+    var data = JSON.stringify({ "id": id, "first": first });
+    xhr.send(data);
+}
