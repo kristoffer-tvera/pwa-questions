e646c3344d61f0d6220ffa3d018208ab78cf2bbd
Kristoffer Tverå <Bluekristoffer@gmail.com>
2018-08-12 18:34:42 +0200

    html for submitting new questions

--- index.php
+++ index.php
@@ -42,33 +42,48 @@
 </div>
 
     <div class="popMenu" id="mainMenu" data-menu-id="0">
-        <a class="popMenu__options" href="?pepelord">
+        <a class="popMenu__options" onclick="menuOpen(1)" href="#">
             <h4>Ask a Question!</h4>
         </a>
+        <form action="/vg" method="post" class="popMenu" data-menu-id="1" id="xxx" onSubmit="return newQuestionFormSubmit(this)">
+            <input type="text" name="category" class="new-question__textBox" placeholder="Question category">
+            <textarea name="first" class="new-question__textArea" placeholder="First Option"></textarea>
+            <textarea name="second" class="new-question__textArea" placeholder="Second Option"></textarea>
+            <!-- <a class="popMenu__options" onclick="menuClose(this)" href="#">
+                <h4>Send</h4>
+            </a> -->
+
+            <button class="popMenu__options" type="submit">
+                Send
+            </button>
+
+            <a class="popMenu__options" onclick="menuClose(this)" href="#">
+                <h4>Back</h4>
+            </a>
+        </form>
         <!-- <div class="popMenu__options"><h4>My Questions</h4></div> -->
-        <a class="popMenu__options" onclick="menuOpen(1)" href="#">
+        <a class="popMenu__options" onclick="menuOpen(2)" href="#">
             <h4>Categories</h4>
         </a>
-        <div class="popMenu" data-menu-id="1" data-category-list="true">
+        <div class="popMenu" data-menu-id="2" data-category-list="true">
             <a class="popMenu__options" onclick="menuClose(this)" href="#">
                 <h4>Back</h4>
             </a>
         </div>
         <!-- <div class="popMenu__options"><h4>Friends</h4></div> -->
-        <a class="popMenu__options" onclick="menuOpen(2)" href="#">
+        <a class="popMenu__options" onclick="menuOpen(3)" href="#">
             <h4>Options</h4>
         </a>
 
-        <div class="popMenu" data-menu-id="2">
-                <a class="popMenu__options" onclick="menuClose(this)" href="#">
-                    <h4>Back</h4>
-                </a>
+        <div class="popMenu" data-menu-id="3">
+            <a class="popMenu__options" onclick="menuClose(this)" href="#">
+                <h4>Back</h4>
+            </a>
 
-                <div class="popMenu__options">
-                    <h4>options here?</h4>
-                </div>
-
+            <div class="popMenu__options">
+                <h4>options here?</h4>
             </div>
+        </div>
     </div>
 
     <!-- Optional JavaScript -->
